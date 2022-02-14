fc1904c80c0ff550ed7dbba2f29344fd4ba89d11
Isaguiar <79108640+Isaguiar@users.noreply.github.com>
2022-02-13 21:39:47 -0300

    Correct open function

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -9,6 +9,6 @@ export default class Page {
     */
     open(path) {
         //return browser.url(`https://the-internet.herokuapp.com/${path}`)
-        return browser.url(`https://www.saucedemo.com/`)
+        return browser.url(`https://www.saucedemo.com/${path}`)
     }
 }
